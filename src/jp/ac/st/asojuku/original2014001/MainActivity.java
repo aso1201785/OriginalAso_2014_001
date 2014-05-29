7a07e33357412f1a52f13490faff6bbacc02e123
student <student@PC633S32.fgakusei.local>
2014-05-29 12:45:20 +0900

    5/29提出課題

---
+++ src/jp/ac/st/asojuku/original2014001/MainActivity.java
@@ -0,0 +1,89 @@
+package jp.ac.st.asojuku.original2014001;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		//ボタンをIDで探してボタン変数をつくる
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		// ボタン変数にリスナーを登録する
+		btnOK.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()){ //どのボタンが押されたか判定
+		case R.id.btnOK: //btnOKが押された
+
+			//Randomクラスのインスタンスを作る
+			Random rnd = new Random();
+			//0～3の4パターンの数字をranに取得
+			int ran = rnd.nextInt(4);
+
+			// 生成して代入用のIntentインスタンス変数を用意
+			Intent intent = null;
+			// ranの値によって処理をわける
+			switch(ran){
+			case 0:
+				// 0なら大吉のページへ飛ばす//
+				// インテントのインスタンス作成
+				intent = new Intent(MainActivity.this, DaikichiActivity.class);
+				//次画面のアクティビティ起動
+				startActivity(intent);
+				break;
+			case 1:
+				// 1なら中吉のページへ飛ばす//
+				// インテントのインスタンス作成
+				intent = new Intent(MainActivity.this, ChuukichiActivity.class);
+				//次画面のアクティビティ起動
+				startActivity(intent);
+				break;
+			case 2:
+				// 2なら凶のページへ飛ばす//
+				// インテントのインスタンス作成
+				intent = new Intent(MainActivity.this, KyouActivity.class);
+				//次画面のアクティビティ起動
+				startActivity(intent);
+				break;
+			case 3:
+				// 3なら大凶のページへ飛ばす//
+				// インテントのインスタンス作成
+				intent = new Intent(MainActivity.this, DaikyouActivity.class);
+				//次画面のアクティビティ起動
+				startActivity(intent);
+				break;
+
+			}
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+
+}
